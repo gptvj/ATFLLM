eb0a435778e26d565fd20da12c9e2d9964245661
Trung <gptvj54@gmail.com>
2024-11-27 07:08:06 +0000

    feat: install lib

--- install_lib.sh
+++ install_lib.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+# Cài đặt các thư viện cần thiết
 pip install datasets
 pip install rank_bm25
 pip install fugashi
@@ -5,11 +8,13 @@ pip install mecab-python3
 pip install unidic-lite
 pip3 install torch torchvision torchaudio
 pip install transformers
-cd tevatron
-export PYTHONPATH=$PYTHONPATH:$(pwd)
-cd ..
 pip install peft
 pip install bitsandbytes
 pip install xformers
 pip install faiss-gpu==1.7.2
 pip install prettytable
+
+# Thêm thư mục tevatron vào PYTHONPATH
+cd tevatron
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+cd ..
